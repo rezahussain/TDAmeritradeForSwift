f47ef6b079bbc30371ae5d3753c88eda9a7bfca1
Reza Hussain <crownonfire@gmail.com>
2021-07-18 11:03:22 -0500

    separating core func and convenience funcs

---
+++ Sources/TDAmeritradeForSwift/Convenience.swift
@@ -0,0 +1,78 @@
+//
+//  File.swift
+//  
+//
+//  Created by admin on 7/18/21.
+//
+
+import Foundation
+
+
+extension TDAmeritradeForSwift
+{
+    public class func doOrderFillOrKillImitation(tdAmeritradeAccountNumber:Int,accessTokenToUse:String,quantity:Int,symbol:String,limitPrice:Optional<Decimal>,timeLimitSecondsForFill:UInt,orderType:orderTypeEnum)->Optional<Order>
+    {
+        
+        var someOrder:Optional<Order> = doOrder(tdAmeritradeAccountNumber:tdAmeritradeAccountNumber,accessTokenToUse:accessTokenToUse,quantity:quantity,symbol:symbol,limitPrice:limitPrice,orderType:orderType)
+        
+        if someOrder != nil
+        {
+            let startDate = Date()
+            
+            var didFill:Bool = false
+            
+            while Date().timeIntervalSince(startDate) < Double(timeLimitSecondsForFill)
+            {
+                someOrder!.refresh(tdAmeritradeAccountNumber: tdAmeritradeAccountNumber, accessTokenToUse: accessTokenToUse)
+                if someOrder!.status!.compare("FILLED") == .orderedSame
+                {
+                    didFill = true
+                    break
+                }
+                if someOrder!.status!.compare("REJECTED") == .orderedSame
+                {
+                    didFill = false
+                    break
+                }
+                sleep(1)
+            }
+            
+            //the reasoning for this line is, right before the cancel is attempted
+            //the order can fill, within that one second gap above
+            //I have observed it happening once so far in ~100 trades near low volume times of the day
+            someOrder!.refresh(tdAmeritradeAccountNumber: tdAmeritradeAccountNumber, accessTokenToUse: accessTokenToUse)
+            
+            if didFill
+            {
+                
+            }
+            else
+            {
+                if someOrder!.cancelable! == true
+                {
+                    someOrder!.cancel(tdAmeritradeAccountNumber: tdAmeritradeAccountNumber, accessTokenToUse: accessTokenToUse)
+                    
+                    var tries:Int = 0
+                    while someOrder!.status!.compare("CANCELED") != .orderedSame && someOrder!.status!.compare("FILLED") != .orderedSame
+                    {
+                        someOrder!.refresh(tdAmeritradeAccountNumber: tdAmeritradeAccountNumber, accessTokenToUse: accessTokenToUse)
+                        sleep(1)
+                        tries = tries + 1
+                        if tries > 5
+                        {
+                            break
+                        }
+                    }
+                }
+                
+            }
+            
+            
+        }
+        
+        return someOrder
+        
+    }
+    
+}
+
