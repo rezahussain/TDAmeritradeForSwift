b3275b05ba0cce0fec5e85ed6525af22af56bf94
admin <admin@admins-MacBook-Air.local>
2022-08-02 14:20:15 -0500

    compiling on m1 bs

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TDAmeritradeForSwift",
+    platforms: [
+          .macOS(.v10_15)
+      ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name:"PerfectHTTPServer",url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0")
+        .package(url: "https://github.com/rezahussain/Perfect-HTTPServer", branch: "master")
         
     ],
     targets: [
@@ -22,7 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TDAmeritradeForSwift",
-            dependencies: ["PerfectHTTPServer"]),
+            dependencies: [.product(name: "PerfectHTTPServer", package: "Perfect-HTTPServer")]),
         .testTarget(
             name: "TDAmeritradeForSwiftTests",
             dependencies: ["TDAmeritradeForSwift"]),
